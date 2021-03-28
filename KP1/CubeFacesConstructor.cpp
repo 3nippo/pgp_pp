6773d3b31885f5953ba2f07ebda336c8206dbcda
3nippo <mopsyaka13@gmail.com>
2021-03-29 00:13:24 +0300

    Implement CubeFacesConstructor

---
+++ KP1/CubeFacesConstructor.cpp
@@ -0,0 +1,52 @@
+#include "CubeFacesConstructor.hpp"
+#include "SquareFace.hpp"
+#include <cmath>
+
+namespace RayTracing
+{
+
+void CubeFacesConstructor::ConstructFaces(
+    std::vector<Face> &faces, 
+    const Point3 &origin, 
+    const float radius
+)
+{
+    float halfA = radius / sqrtf(3);
+    
+    // front face
+    faces.emplace_back(
+        origin + Point3{ -halfA, -halfA, +halfA },
+        origin + Point3{ +halfA, -halfA, +halfA },
+        origin + Point3{ +halfA, +halfA, +halfA }
+    );
+
+    // back face
+    faces.emplace_back(
+        origin + Point3{ -halfA, -halfA, -halfA },
+        origin + Point3{ +halfA, -halfA, -halfA },
+        origin + Point3{ +halfA, +halfA, -halfA }
+    );
+
+    // left face
+    faces.emplace_back(
+        origin + Point3{ -halfA, -halfA, +halfA },
+        origin + Point3{ -halfA, -halfA, -halfA },
+        origin + Point3{ -halfA, +halfA, -halfA }
+    );
+    
+    // right
+    faces.emplace_back(
+        origin + Point3{ +halfA, -halfA, +halfA },
+        origin + Point3{ +halfA, -halfA, -halfA },
+        origin + Point3{ +halfA, +halfA, -halfA }
+    );
+    
+    // bottom face
+    faces.emplace_back(
+        origin + Point3{ +halfA, -halfA, +halfA },
+        origin + Point3{ +halfA, -halfA, -halfA },
+        origin + Point3{ -halfA, -halfA, -halfA }
+    );
+}
+
+} // namespace RayTracing
