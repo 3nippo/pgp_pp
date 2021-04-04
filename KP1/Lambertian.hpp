00471b4c282c2f54d3bd962121f2b00177990c2b
3nippo <mopsyaka13@gmail.com>
2021-04-04 17:12:03 +0300

    Impl Lambertian

---
+++ KP1/Lambertian.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "Material.hpp"
+#include "HitRecord.hpp"
+#include "Ray.hpp"
+#include "utils.hpp"
+
+namespace RayTracing
+{
+
+class Lambertian : public Material
+{
+private:
+    Color m_albedo;
+public:
+    Lambertian(const Color &albedo)
+        : m_albedo(albedo)
+    {}
+private:
+    static Vector3 RandomVectorInUnitSphere()
+    {
+        while (true)
+        {
+            const Vector3 v = Vector3::Random(-1, 1);
+
+            if (v.LengthSquared() >= 1)
+                continue;
+
+            return v;
+        }
+    }
+
+    static Vector3 LambertianRandomVector() 
+    {
+        return RandomVectorInUnitSphere().UnitVector();
+    }
+
+    virtual bool scatter(
+        const Ray &ray,
+        const HitRecord &hitRecord,
+        Color &attenuation,
+        Ray &scattered
+    ) const override
+    {
+        Vector3 scatterDir = hitRecord.normal + LambertianRandomVector();
+
+        if (scatterDir.NearZero())
+            scatterDir = hitRecord.normal;
+
+        scattered = Ray(
+            hitRecord.point,
+            scatterDir
+        );
+
+        attenuation = m_albedo;
+
+        return true;
+    }
+};
+
+} // namespace RayTracing
