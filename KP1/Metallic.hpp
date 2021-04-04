19b517757e72b15400e8c9544bc2990376337f58
3nippo <mopsyaka13@gmail.com>
2021-04-04 18:09:22 +0300

    Impl metallic

---
+++ KP1/Metallic.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Material.hpp"
+#include "HitRecord.hpp"
+#include "Ray.hpp"
+#include "utils.hpp"
+
+namespace RayTracing
+{
+
+class Metallic : public Material
+{
+private:
+    Color m_albedo;
+public:
+    Metallic(const Color &albedo)
+        : m_albedo(albedo)
+    {}
+private:
+    static Vector3 RandomVectorInUnitSphere()
+    {
+        while (true)
+        {
+            const Vector3 v = Vector3::Random(-1, 1);
+
+            if (v.LengthSquared() >= 1)
+                continue;
+
+            return v;
+        }
+    }
+
+    static Vector3 MetallicRandomVector() 
+    {
+        return RandomVectorInUnitSphere().UnitVector();
+    }
+
+    virtual bool scatter(
+        const Ray &ray,
+        const HitRecord &hitRecord,
+        Color &attenuation,
+        Ray &scattered
+    ) const override
+    {
+        const Vector3 reflected = Vector3::Reflect(
+            ray.direction.UnitVector(), 
+            hitRecord.normal
+        );
+
+        scattered = Ray(hitRecord.point, reflected);
+        attenuation = m_albedo;
+
+        return hitRecord.normal.Dot(scattered.direction) > 0;
+    }
+};
+
+} // namespace RayTracing
