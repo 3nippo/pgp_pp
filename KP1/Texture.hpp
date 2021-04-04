656ea437b8a8f1322794ec8a37ab9cd75757e08e
3nippo <mopsyaka13@gmail.com>
2021-04-04 22:37:58 +0300

    Impl texture

---
+++ KP1/Texture.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Vector3.hpp"
+
+namespace RayTracing
+{
+class Texture
+{
+public:
+    virtual Color GetColor(const float u, const float v) const = 0;
+};
+
+class SolidTexture : Texture
+{
+private:
+    Color m_color;
+public:
+    SolidTexture(const Color &color)
+        : m_color(color)
+    {}
+
+    virtual Color GetColor(const float u, const float v) const override
+    {
+        return m_color;
+    }
+};
+}; // namespace RayTracing
