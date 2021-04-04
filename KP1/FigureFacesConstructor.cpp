edbc9a8e876a66f1cd4efcb8dad0f45876f35ed4
3nippo <mopsyaka13@gmail.com>
2021-04-05 00:38:45 +0300

    Impl FacesConstructor for TexturedCube

--- KP1/FigureFacesConstructor.cpp
+++ KP1/FigureFacesConstructor.cpp
@@ -1,6 +1,7 @@
 #include "FigureFacesConstructor.hpp"
 #include "SquareFace.hpp"
 #include <cmath>
+#include "Texture.hpp"
 
 namespace RayTracing
 {
@@ -70,4 +71,128 @@ void FigureFacesConstructor::ConstructFigureFaces
     );
 }
 
+template<>
+void FigureFacesConstructor::ConstructFigureFaces
+(
+    std::vector<MappedSquareFace> &faces,
+    const Point3 &origin,
+    const float radius
+)
+{
+    float halfA = radius / sqrtf(3);
+    
+    // front face
+    faces.emplace_back(
+        Point3{ -halfA, -halfA, +halfA },
+        Point3{ +halfA, -halfA, +halfA },
+        Point3{ +halfA, +halfA, +halfA },
+        Point3{ -halfA, +halfA, +halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+
+    // back face
+    faces.emplace_back(
+        Point3{ -halfA, -halfA, -halfA },
+        Point3{ +halfA, -halfA, -halfA },
+        Point3{ +halfA, +halfA, -halfA },
+        Point3{ -halfA, +halfA, -halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+
+    // left face
+    faces.emplace_back(
+        Point3{ -halfA, -halfA, -halfA },
+        Point3{ -halfA, +halfA, -halfA },
+        Point3{ -halfA, +halfA, +halfA },
+        Point3{ -halfA, -halfA, +halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+    
+    // right face
+    faces.emplace_back(
+        Point3{ +halfA, -halfA, -halfA },
+        Point3{ +halfA, +halfA, -halfA },
+        Point3{ +halfA, +halfA, +halfA },
+        Point3{ +halfA, -halfA, +halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+    
+    // bottom face
+    faces.emplace_back(
+        Point3{ -halfA, -halfA, -halfA },
+        Point3{ +halfA, -halfA, -halfA },
+        Point3{ +halfA, -halfA, +halfA },
+        Point3{ -halfA, -halfA, +halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+
+    // top face
+    faces.emplace_back(
+        Point3{ -halfA, +halfA, -halfA },
+        Point3{ +halfA, +halfA, -halfA },
+        Point3{ +halfA, +halfA, +halfA },
+        Point3{ -halfA, +halfA, +halfA },
+        origin,
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 1, 1, 0 },
+            Point3{ 1, 0, 0 }
+        },
+        TriangleMapping{
+            Point3{ 0, 1, 0 },
+            Point3{ 0, 0, 0 },
+            Point3{ 1, 0, 0 }
+        }
+    );
+}
 } // namespace RayTracing
