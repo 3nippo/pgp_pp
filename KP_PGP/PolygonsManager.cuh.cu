d10ad4b1c62a1ac3e5abbe2b4890d4f5c1f5b7a8
3nippo <mopsyaka13@gmail.com>
2021-05-15 11:53:08 +0300

    Remove const

--- KP_PGP/PolygonsManager.cuh.cu
+++ KP_PGP/PolygonsManager.cuh.cu
@@ -47,7 +47,7 @@ public:
         const Point3 &C,
         const Point3 &D,
         const Point3 &origin,
-        const Material * const * const material
+        Material * const * const material
     )
     {
         AddPolygon(
@@ -69,7 +69,7 @@ public:
         const Point3 &C,
         const Point3 &D,
         const Point3 &origin,
-        const Material * const * const material,
+        Material * const * const material,
         const TriangleMapping &mapping1,
         const TriangleMapping &mapping2
     )
